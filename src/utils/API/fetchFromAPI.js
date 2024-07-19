de1b012f68fb017948d4e2413f713983cdee8b26
box <box>
2024-07-19 19:25:53 +0545

    axios with credentials

--- src/utils/API/fetchFromAPI.js
+++ src/utils/API/fetchFromAPI.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const BASE_URL = 'http://localhost:5000/api/v1'
 
-const fetchFromAPI = async(endpoint, options={})=>{
+const fetchFromAPI = async(endpoint)=>{
     try{
-        const { data } = await axios.get(`${BASE_URL}${endpoint}`, options);
+        const { data } = await axios.get(`${BASE_URL}${endpoint}`, {withCredentials: true});
         return data;
     } catch(err){
         throw err;
--- src/utils/API/postToAPI.js
+++ src/utils/API/postToAPI.js
@@ -5,7 +5,7 @@ const BASE_URL = 'http://localhost:5000/api/v1'
 
 const postToAPI = async(endpoint, postData)=>{
     try{
-        const { data } = await axios.post(`${BASE_URL}${endpoint}`, postData);
+        const { data } = await axios.post(`${BASE_URL}${endpoint}`, postData, {withCredentials: true});
         return data;
     } catch(error){
         throw new Error(error.response.status, error.response.data);
