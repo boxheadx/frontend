5b23a2b49df7ecbea1b9ec059f792ce27ce4cb28
owl <owl>
2024-08-09 16:52:45 +0545

    fixed top books and added loading screen because render is too frikin slow

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Sidebar, Books } from './';
 import { fetchFromAPI } from '../utils/API';
 import { useNavigate } from 'react-router-dom';
+import { Backdrop, CircularProgress } from '@mui/material';
 
 const Home = ({ user, setUser }) => {
     const [latestBooks, setLatestBooks] = useState([]);
+    const [topBooks, setTopBooks] = useState([]);
     const [isSidebarVisible, setSidebarVisible] = useState(true);
     const [selectedBook, setSelectedBook] = useState(null);
 
@@ -25,6 +27,11 @@ const Home = ({ user, setUser }) => {
         fetchFromAPI('/book/latest').then((data) => {
             setLatestBooks(data);
         });
+
+        fetchFromAPI('/book/top').then((data) => {
+            setTopBooks(data);
+        });
+
         if(!user){
             setSidebarVisible(false);
         }
@@ -46,10 +53,15 @@ const Home = ({ user, setUser }) => {
             {/* <div className={`content ${isSidebarVisible ? 'sidebar-visible' : 'sidebar-hidden'}`}> */}
                 <div className='browse-content'>
                 <p className='list-title'>Latest</p>
+                {!latestBooks && <p className='loading'><Backdrop
+                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                    open>
+                    <CircularProgress color="inherit" />
+                    </Backdrop></p>}
                 <Books books={latestBooks} handleBookSelect={handleBookSelect} />
                 <hr style={{width: `${latestBooks.length * 100}px`}}/>
                 <p className='list-title'>Top Rated</p>
-                <Books books={latestBooks} handleBookSelect={handleBookSelect} />
+                <Books books={topBooks} handleBookSelect={handleBookSelect} />
             </div>
         </div>
     );
