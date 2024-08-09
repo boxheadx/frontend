d4afbcaba90b14d83cf458b5c3dcf8dfa6089ec8
owl <owl>
2024-08-09 17:36:32 +0545

    fixed loading issue i think

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -53,11 +53,11 @@ const Home = ({ user, setUser }) => {
             {/* <div className={`content ${isSidebarVisible ? 'sidebar-visible' : 'sidebar-hidden'}`}> */}
                 <div className='browse-content'>
                 <p className='list-title'>Latest</p>
-                {!latestBooks && <p className='loading'><Backdrop
+                {!latestBooks && <Backdrop
                     sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
                     open>
                     <CircularProgress color="inherit" />
-                    </Backdrop></p>}
+                    </Backdrop>}
                 <Books books={latestBooks} handleBookSelect={handleBookSelect} />
                 <hr style={{width: `${latestBooks.length * 100}px`}}/>
                 <p className='list-title'>Top Rated</p>
